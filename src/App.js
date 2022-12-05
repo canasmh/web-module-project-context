d67d927c98a001942e8f8a8818161e836641e7c1
canasmh <canasmh@yahoo.com>
2022-12-05 13:09:43 -0500

    Create a ProductContext and import it into App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { set } from 'msw/lib/types/context';
+import { ProductContext } from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart(...cart, item)
 	};
 
 	return (
